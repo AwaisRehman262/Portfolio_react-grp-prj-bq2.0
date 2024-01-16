cbcadd85dbe2e4fd256f1a2e7ca9e3d518277240
AwaisRehman262 <aw2946652@gmail.com>
2024-01-16 15:02:09 +0500

    don;t know

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import Navbar from './components/common/Navbar/index.jsx';
-import './App.css';
-import './components/common/Navbar/navbar.module.css';
-import SectionTwo from './components/features/Section-Two/index.jsx';
-import SectionOne from './components/features/Section-One/index.jsx';
-import SectionThree from './components/features/Section-Three/index.jsx';
-import AwaisComponent from './components/features/Awais Components/index.jsx';
-import { SectionSeven } from './components/features/Zain code/Section-Seven.jsx';
-import Footer from './components/common/Footer/main.jsx';
+import React from "react";
+import Navbar from "./components/common/Navbar/index.jsx";
+import "./App.css";
+import "./components/common/Navbar/navbar.module.css";
+import SectionTwo from "./components/features/Section-Two/index.jsx";
+import SectionOne from "./components/features/Section-One/index.jsx";
+import SectionThree from "./components/features/Section-Three/index.jsx";
+import AwaisComponent from "./components/features/Awais Components/index.jsx";
+import { SectionSeven } from "./components/features/Zain code/Section-Seven.jsx";
+import Footer from "./components/common/Footer/main.jsx";
 
 const App = () => {
   return (
     <>
       <Navbar />
-        <div className='container'>
-          <SectionOne />
-          <SectionTwo />
-          <SectionThree />
-          <AwaisComponent />
-          <SectionSeven />
-        </div>
+      <div className="container">
+        <SectionOne />
+        <SectionTwo />
+        <SectionThree />
+        <AwaisComponent />
+        <SectionSeven />
+      </div>
       <Footer />
     </>
   );
